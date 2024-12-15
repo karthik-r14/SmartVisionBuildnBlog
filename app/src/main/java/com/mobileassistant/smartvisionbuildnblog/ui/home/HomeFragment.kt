405d668a597d523509e43a976fef6028e6bcecf9
Karthik Ramachandran <karthikr1493@gmail.com>
2024-12-15 21:22:36 +0530

    Create Home Fragment.

--- app/src/main/java/com/mobileassistant/smartvisionbuildnblog/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mobileassistant/smartvisionbuildnblog/ui/home/HomeFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.mobileassistant.smartvisionbuildnblog.R
 import com.mobileassistant.smartvisionbuildnblog.databinding.FragmentHomeBinding
+import com.mobileassistant.smartvisionbuildnblog.model.MenuItem
+import com.mobileassistant.smartvisionbuildnblogui.home.DashboardAdapter
+
+private const val NO_OF_COLUMNS = 2
 
 class HomeFragment : Fragment() {
 
@@ -18,22 +23,39 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        setupUi()
         return root
     }
+
+    private fun setupUi() {
+        with(binding.recyclerView) {
+            layoutManager = GridLayoutManager(context, NO_OF_COLUMNS)
+            val items: List<MenuItem> = listOf(
+                MenuItem(
+                    getString(R.string.menu_item_3),
+                    R.drawable.object_detection_icon,
+                    ::navigateToDetectObjects
+                ), MenuItem(
+                    getString(R.string.menu_item_5),
+                    R.drawable.barcode_code_scanner,
+                    ::navigateToBarcodeScanner
+                )
+            )
+            val adapter = DashboardAdapter(context, items)
+            setAdapter(adapter)
+        }
+    }
+
+    private fun navigateToDetectObjects() = findNavController().navigate(R.id.nav_object_detection)
+
+    private fun navigateToBarcodeScanner() = findNavController().navigate(R.id.nav_scan_code)
+
 
     override fun onDestroyView() {
         super.onDestroyView()
