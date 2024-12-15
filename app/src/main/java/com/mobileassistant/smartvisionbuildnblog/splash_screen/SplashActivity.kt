da3ec59837a80ec2e7ea523a5b15e09b92e15dcc
Karthik Ramachandran <karthikr1493@gmail.com>
2024-12-15 21:19:43 +0530

    Add splash screen

---
+++ app/src/main/java/com/mobileassistant/smartvisionbuildnblog/splash_screen/SplashActivity.kt
@@ -0,0 +1,55 @@
+package com.mobileassistant.smartvisionbuildnblog.splash_screen
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.mobileassistant.smartvisionbuildnblogMainActivity
+import com.mobileassistant.smartvisionbuildnblogsplash_screen.ui.theme.SmartVisionTheme
+
+class SplashActivity : ComponentActivity() {
+    private val viewModel: SplashViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            this.setKeepOnScreenCondition { viewModel.isLoading.value }
+        }
+
+        setContent {
+            SmartVisionTheme {
+                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SmartVisionTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/mobileassistant/smartvisionbuildnblog/splash_screen/SplashViewModel.kt
@@ -0,0 +1,21 @@
+package com.mobileassistant.smartvisionbuildnblogsplash_screen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+const val SPLASH_SCREEN_DURATION = 3000L
+class SplashViewModel : ViewModel() {
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(SPLASH_SCREEN_DURATION)
+            _isLoading.value = false
+        }
+    }
+}
